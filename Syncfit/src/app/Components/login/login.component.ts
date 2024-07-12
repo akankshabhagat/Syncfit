bf936ead7c590dc23a13157717199ce3d4fff2ca
akanksha <akankshabhagat606@gmail.com>
2024-07-12 21:01:09 +0530

    figuring out express ,mongodb connection

--- Syncfit/src/app/Components/login/login.component.ts
+++ Syncfit/src/app/Components/login/login.component.ts
@@ -6,19 +6,32 @@ import {
   Validators,
   FormsModule,
   ReactiveFormsModule,
+  
 } from '@angular/forms';
 import {ChangeDetectionStrategy, signal} from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 import { ErrorStateMatcher } from '@angular/material/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'login',
   standalone: true,
-  imports: [FormsModule,MatFormFieldModule,MatInputModule,MatToolbarModule,ReactiveFormsModule,MatSelectModule],
+  imports: [CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatCheckboxModule
+    ,MatIconModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -26,14 +39,18 @@ import {MatSelectModule} from '@angular/material/select';
 })
 
 export class LoginComponent {
-  protected readonly value = signal('');
+  value=' ';
+  // protected readonly value = signal('');
  
-  protected onInput(event: Event) {
-    this.value.set((event.target as HTMLInputElement).value);
+  // protected onInput(event: Event) {
+  //   this.value.set((event.target as HTMLInputElement).value);
 
+  // }
+  usernameFC=new FormControl('',[Validators.required]);
+  workouts = new FormControl<string[]>([], { validators: Validators.required, nonNullable: true });
+  workoutList: string[] = ['Cycling', 'Jogging', 'Swimming', 'Yoga', 'Other'];
+
+  get selectedWorkouts(): string {
+    return this.workouts.value.join(', ');
   }
-workouts = new FormControl(Validators.required);
-  
-workoutList: string[]=['Cycling','Jogging','Swimming','Yoga','Other'];
-
 }
